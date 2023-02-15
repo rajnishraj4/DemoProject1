b6a0bf069ee5e475dd61ddbf8dff2b5e8c4e35f2
Rajnish Raj <rajnishraj4@gmail.com>
2023-02-15 23:30:33 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DemoProject1';
+  title = 'Welcome to the developers hub!';
 }
